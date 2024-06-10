63bbff0d4afa57f10bb4e66f720f5525022cabc5
lancelooottt <lance_mendoza08@yahoo.com>
2024-06-10 20:59:50 +0800

    first try

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,33 @@
+import { View, Text, FlatList } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Home = () => {
+  return (
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-center flex-row mb-6">
+              <View>
+                <Text className="font-medium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  JS Mastery
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
